f1c4f2064ff7b2e3dff1b320ab05e689372c7b5d
NitayNahary <nitay.nahary@campus.technion.ac.il>
2022-06-12 15:51:35 +0300

    testing ver 4.1.3 new exception implement

--- Exception.h
+++ Exception.h
@@ -4,16 +4,16 @@
 #include <string>
 
 // The base class for exceptions
-static const std::string DECK_NOT_FOUND ="";
+static const char* DECK_NOT_FOUND ="";
 class exception {
 public:
     virtual ~exception() = default;
-    virtual std::string what() const = 0;
+    virtual const char* what() const = 0;
 };
 
 // deck file not found exception
 class DeckFileNotFound : public exception {
-    std::string what() const override{
+    const char* what() const override{
         return "Deck File Error: File not found";
     }
 };
@@ -24,42 +24,43 @@ class DeckFileFormatError : public exception {
     int m_lineNumber;
 public:
     DeckFileFormatError(int lineNumber) : m_lineNumber(lineNumber){}
-    std::string what() const override{
-        return (std::string("Deck File Error: File format error in line ") + std::to_string(m_lineNumber));
+    const char* what() const override{
+        const std::string result = std::string("Deck File Error: File format error in line ") + std::to_string(m_lineNumber);
+        return result.c_str();
     }
 };
 
 // Invalid size of deck file exception
 class DeckFileInvalidSize : public exception {
-    std::string what() const override{
+    const char* what() const override{
         return "Deck File Error: Deck size is invalid";
     }
 };
 
 // Invalid name exception
 class InvalidName : public exception{
-    std::string what() const override{
+    const char* what() const override{
         return "Invalid Name";
     }
 };
 
 // End of file exception
 class EndOfFile : public exception{
-    std::string what() const override{
+    const char* what() const override{
         return "End Of File";
     }
 };
 
 // General error exception
 class GeneralError : public exception{
-    std::string what() const override{
+    const char* what() const override{
         return "Reason Unknown";
     }
 };
 
 // Invalid team players input exception
 class InvalidTeamInput : public exception{
-    std::string what() const override{
+    const char* what() const override{
         return "Invalid team players input";
     }
 };
