0433f6aff340a8c485e472fcd7185138f6b880ea
NitayNahary <nitay.nahary@campus.technion.ac.il>
2022-06-23 14:54:48 +0300

    testing- expansion ver 5.3.0 Commenting and Polishing

--- Cards/Gang.cpp
+++ Cards/Gang.cpp
@@ -8,7 +8,7 @@ Gang::Gang() {
     m_open = true;
 }
 
-Gang::Gang(const Gang &src) : Card() , m_open(src.m_open){
+Gang::Gang(const Gang &src) : Card() , m_open(src.m_open), m_gang(){
     for(const std::unique_ptr<MonsterCard>& monster : src.m_gang){
         if(dynamic_cast<Dragon*>(&*monster)){
             m_gang.push_back(std::unique_ptr<MonsterCard>(new Dragon()));
